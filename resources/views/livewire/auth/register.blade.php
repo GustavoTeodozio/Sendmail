78920af2319558e0cb0213172c1da2742bc53b14
Gustavo Teodozio <108893339+GustavoTeodozio@users.noreply.github.com>
2025-03-18 18:35:51 -0300

    comentario

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -9,33 +9,33 @@ use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
 new #[Layout('components.layouts.auth')] class extends Component {
-    public string $name = '';
-    public string $email = '';
-    public string $password = '';
-    public string $password_confirmation = '';
+    // public string $name = '';
+    // public string $email = '';
+    // public string $password = '';
+    // public string $password_confirmation = '';
 
     /**
      * Handle an incoming registration request.
      */
-    public function register(): void
-    {
-        $validated = $this->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-            'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
-        ]);
+    // public function register(): void
+    // {
+    //     $validated = $this->validate([
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+    //         'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
+    //     ]);
 
-        $validated['password'] = Hash::make($validated['password']);
+    //     $validated['password'] = Hash::make($validated['password']);
 
-        event(new Registered(($user = User::create($validated))));
+    //     event(new Registered(($user = User::create($validated))));
 
-        Auth::login($user);
+    //     Auth::login($user);
 
-        $this->redirectIntended(route('dashboard', absolute: false), navigate: true);
-    }
+    //     $this->redirectIntended(route('dashboard', absolute: false), navigate: true);
+    // }
 }; ?>
 
-<div class="flex flex-col gap-6">
+{{-- <div class="flex flex-col gap-6">
     <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
 
     <!-- Session Status -->
@@ -94,4 +94,4 @@ new #[Layout('components.layouts.auth')] class extends Component {
         {{ __('Already have an account?') }}
         <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
     </div>
-</div>
+</div> --}}
